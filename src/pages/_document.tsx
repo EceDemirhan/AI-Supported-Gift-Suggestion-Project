cdbb1185a5a528e7d5583bdf2f41d4ec34de86cd
EceDemirhan <141672775+EceDemirhan@users.noreply.github.com>
2025-08-26 21:34:28 +0300

    akış trafiği eklendi.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,24 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { AppConfig } from '../utils/AppConfig';
 
-// Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          {/* Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-8T18Z062E"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-G8T18Z062E');
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
